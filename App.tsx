43dbc01da4c50e891061df0b2e97ed44a4ce557e
iMrHoms <obr_dev@protonmail.com>
2026-06-27 19:51:09 -0600

    chore: Project setup

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Here we go again!</Text>
       <StatusBar style="auto" />
     </View>
   );
